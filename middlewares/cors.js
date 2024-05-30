0cda1788491afbc24b048b71e7b86e1a33f8f749
mentalNika <mandrygina798@mail.ru>
2024-05-31 00:10:32 +0300

    Change cors settings

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,6 +10,7 @@ function cors(req, res, next) {
 
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
   }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header(
