fc9305fc2fb53b919168ba3fd344bdd0d81a0489
mentalNika <mandrygina798@mail.ru>
2024-05-31 07:01:36 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,21 @@
+const allowedCors = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://nika.nomorepartiesco.ru",
+  "https://love.nomorepartiesco.ru",
+];
+
 function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
+
   next();
 }
-
-module.exports = cors;
